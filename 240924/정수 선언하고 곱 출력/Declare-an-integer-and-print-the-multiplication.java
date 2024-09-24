99185ccf071b0725fdbe1859fe918e764af30c10
김재형 <127040847+Joopy-KR@users.noreply.github.com>
2024-09-24 11:00:08 +0900

    Title: 정수 선언하고 곱 출력, Time: 83ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240924/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 26;
+        int b = 5;
+        System.out.println(String.format("%d * %d = %d", a, b, a * b));
+    }
+}
