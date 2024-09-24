5fd565cd2002d9116dd6f2060d6967559de74e1b
김재형 <127040847+Joopy-KR@users.noreply.github.com>
2024-09-24 11:30:29 +0900

    Title: 길이 단위 변환하기, Time: 93ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240924/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        double ONE_FEET = 30.48;
+        int ONE_MILE = 160934;
+
+        System.out.printf("9.2ft = %.1fcm\n1.3mi = %.1fcm", 9.2 * ONE_FEET, 1.3 * ONE_MILE);
+    }
+}
