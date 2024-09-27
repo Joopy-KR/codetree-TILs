20d47f4c044a7f142fbe69c69c10581aab370ef2
김재형 <127040847+Joopy-KR@users.noreply.github.com>
2024-09-27 23:37:35 +0900

    Title: 시험 통과 여부 확인하기, Time: 88ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240927/시험 통과 여부 확인하기/verify-test-passed.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        if (n > 80) {
+            System.out.println("pass");
+        } 
+        else {
+            System.out.printf("%d more score", 80 - n);
+        }
+    }
+}
