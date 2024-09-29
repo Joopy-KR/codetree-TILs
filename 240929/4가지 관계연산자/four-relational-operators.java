7bb0f6618949256c73efd76d74b63dd1c898af09
김재형 <127040847+Joopy-KR@users.noreply.github.com>
2024-09-29 18:14:42 +0900

    Title: 4가지 관계연산자, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240929/4가지 관계연산자/four-relational-operators.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int[] arr = new int[4];
+        arr[0] = a >= b ? 1 : 0;
+        arr[1] = a > b ? 1 : 0;
+        arr[2] = a <= b ? 1 : 0;
+        arr[3] = a < b ? 1 : 0;
+        for (int ans: arr) {
+            System.out.println(ans);
+        }
+    }
+}
