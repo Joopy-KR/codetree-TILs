ab4dbabfd6a821e9154b7de612a0dbbd97f17c0e
김재형 <127040847+Joopy-KR@users.noreply.github.com>
2024-09-30 15:06:47 +0900

    Title: 코로나 메뉴얼, Time: 84ms, Memory: 8MB, Status: Passed - CodeTree

--- 240930/코로나 메뉴얼/covid-manual.java
+++ 240930/코로나 메뉴얼/covid-manual.java
@@ -6,13 +6,17 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int count = 0;
 
         for (int i = 0; i < 3; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             if (st.nextToken().equals("Y")) {
                 if (Integer.parseInt(st.nextToken()) >= 37) {
-                    System.out.println("E");
-                    return;
+                    count++;
+                    if (count == 2) {
+                        System.out.println("E");
+                        return;   
+                    }
                 }
             }
         }
