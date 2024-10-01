ef1271a5259ffb12d9af2517a70ec56868997629
김재형 <127040847+Joopy-KR@users.noreply.github.com>
2024-10-01 17:42:18 +0900

    Title: 별 n개 출력하기, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241001/별 n개 출력하기/print-n-stars.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int check = 0;
+        while (check < n) {
+            System.out.println("*");
+            check++;
+        }
+    }
+}
